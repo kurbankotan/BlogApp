87b5ed3bd859aa85ed2416a89e24f3abe2fedc13
Kurban KOTAN <kurbankotan@gmail.com>
2024-07-26 15:37:01 +0300

    Adding View Components for Tags

--- obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4fb95ce828ecc0a9ec2a86701acb10c440ab3e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f6b2363c64ca23b95c8481580349f5e26cbc404")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
